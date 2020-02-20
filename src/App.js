f6c2633ff42f19a8eb1b3e552e366f2731d9cdfd
Cadell Christo <cadellchristo>
2020-02-20 16:30:38 +0800

    comitted progress

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+const localStorageKey = "query";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [query, setQuery] = useState(
+		localStorage.getItem(localStorageKey) || ""
+	);
+
+	useEffect(() => {
+		localStorage.setItem(localStorageKey, query);
+	}, [query]);
+
+	const whereMatch = query.match(/\n\nwhere (.*?)\n\ngroup/ims);
+	const whereSection = whereMatch[1];
+	const whereSectionWithoutComments = whereSection.replace(/--(.*?)\n/gi, "");
+
+	const variableRegex = /@(.*?)( |\n)/;
+
+	const variablesArray = whereSectionWithoutComments
+		.match(new RegExp(variableRegex, "g"))
+		.map(rawMatch => rawMatch.match(new RegExp(variableRegex))[1]);
+	const variables = new Set(variablesArray);
+
+	const whereSectionWithVariablesReplaced = whereSectionWithoutComments.replace(
+		new RegExp(variableRegex, "g"),
+		match => `:${match.substring(1)}`
+	);
+
+	const whereConditions = whereSectionWithVariablesReplaced.split(/\n\nand /);
+
+	const javaCodeForVariables = Array.from(variables)
+		.map(variable => `query.setParameter("${variable}", );\n`)
+		.join("");
+
+	const javaCodeForWhereConditions = whereConditions
+		.map(
+			condition =>
+				`conditions.add("${condition
+					.split("\n")
+					.join(' \\n" + \n"')
+					.replace(/"(\t{1,})/g, match => `${match.substring(1)}"`)}");\n`
+		)
+		.join("");
+
+	const javaCode = `
+List<String> conditions = new ArrayList<>();
+
+${javaCodeForWhereConditions}
+
+String whereClause = "where " + conditions.stream().collect(Collectors.joining(" and "));
+
+${javaCodeForVariables}
+	`;
+
+	return (
+		<div className="App">
+			<div style={{ display: "flex", height: "800px" }}>
+				<div style={{ flexGrow: 1 }}>
+					<textarea
+						value={query}
+						onChange={evt => setQuery(evt.currentTarget.value)}
+						style={{ width: "100%", height: "100%" }}
+					></textarea>
+				</div>
+				<div style={{ flexGrow: 1 }}>
+					<textarea
+						defaultValue={javaCode}
+						style={{ width: "100%", height: "100%" }}
+					></textarea>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
